5576e44c9967666976589070f4f0c1d4d803e81e
Swapnil Singh <swa929911@gmail.com>
2025-09-11 22:54:52 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to CarePulse. Monitor and manage patient health efficiently.</p>
+  </div>
+);
+
+export default Home;
