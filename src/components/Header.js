771e4915f7fd10612e1f6b994b475ba10c7f6bb3
Swapnil Singh <swa929911@gmail.com>
2025-09-11 22:51:11 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>CarePulse</h1>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/patients">Patients</a>
+      <a href="/dashboard">Dashboard</a>
+    </nav>
+  </header>
+);
+
+export default Header;
